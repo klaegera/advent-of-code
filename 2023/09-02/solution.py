d8e7760679667bfbe1126ec4694fba8c78e86f03
Adrian Klaeger <adrian.klaeger@gmail.com>
2023-12-11 18:51:00 +0100

    09-02

---
+++ 2023/09-02/solution.py
@@ -0,0 +1,14 @@
+with open("input") as f:
+    input = [line.strip() for line in f.readlines()]
+
+rows = [[int(n) for n in line.split()] for line in input]
+
+
+result = 0
+
+for row in rows:
+    while any(row):
+        result += row[0]
+        row = [a - b for a, b in zip(row, row[1:])]
+
+print(result)
