c644ff4909afe8e50ae844119df6a71b4262bfe0
Adrian Klaeger <adrian.klaeger@scs.ch>
2023-12-15 14:44:29 +0100

    15-02

---
+++ 2023/15-02/solution.py
@@ -0,0 +1,33 @@
+with open("input") as f:
+    input = [line.strip() for line in f.readlines()]
+
+steps = input[0].split(",")
+
+
+boxes = [{} for _ in range(256)]
+for step in steps:
+    if "-" in step:
+        label, focus = step[:-1], None
+    else:
+        label, focus = step[:-2], step[-1]
+
+    hash = 0
+    for c in label:
+        hash += ord(c)
+        hash *= 17
+        hash %= 256
+
+    if focus:
+        if label not in boxes[hash]:
+            boxes[hash][label] = [focus]
+        else:
+            boxes[hash][label][0] = focus
+    else:
+        boxes[hash].pop(label, None)
+
+result = 0
+for b, box in enumerate(boxes):
+    for l, [lens] in enumerate(box.values()):
+        result += (b + 1) * (l + 1) * int(lens)
+
+print(result)
