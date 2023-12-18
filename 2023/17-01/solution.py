0a8ceccc56e76b54d822c9fe3e54cf8864ecf713
Adrian Klaeger <adrian.klaeger@gmail.com>
2023-12-18 15:14:06 +0100

    17-01: streamline

--- 2023/17-01/solution.py
+++ 2023/17-01/solution.py
@@ -6,74 +6,42 @@ with open("input") as f:
 city = [[int(v) for v in row] for row in input]
 
 
+result = float("inf")
 best = {}
+check = [(0, 0, 0, None, 0)]
 
 
-def update_best(cost, r, c, dir, speed, prev):
+def update(cost, r, c, dir, speed):
+    if not (0 <= r < len(city) and 0 <= c < len(city[0])) or speed > 3:
+        return
+
+    cost += city[r][c]
+
+    if (r, c) == (len(city) - 1, len(city[0]) - 1):
+        global result
+        result = min(result, cost)
+        return
+
     updated = False
     for s in range(speed, 4):
         key = r, c, dir, s
-        if key not in best or cost < best[key][0]:
-            best[key] = cost, prev
+        if key not in best or cost < best[key]:
+            best[key] = cost
             updated = True
-    return updated
+    if updated:
+        heappush(check, (cost, r, c, dir, speed))
 
 
-check = [(0, 0, 0, None, 0, None)]
 while check:
-    prev = heappop(check)
-    cost, r, c, dir, speed, _ = prev
+    cost, r, c, dir, speed = heappop(check)
 
-    if dir != "U" and r < len(city) - 1 and not (dir == "D" and speed == 3):
-        next = (
-            cost + city[r + 1][c],
-            r + 1,
-            c,
-            "D",
-            speed + 1 if dir == "D" else 1,
-            prev,
-        )
-        if update_best(*next):
-            heappush(check, next)
-    if dir != "D" and r > 0 and not (dir == "U" and speed == 3):
-        next = (
-            cost + city[r - 1][c],
-            r - 1,
-            c,
-            "U",
-            speed + 1 if dir == "U" else 1,
-            prev,
-        )
-        if update_best(*next):
-            heappush(check, next)
-    if dir != "L" and c < len(city[0]) - 1 and not (dir == "R" and speed == 3):
-        next = (
-            cost + city[r][c + 1],
-            r,
-            c + 1,
-            "R",
-            speed + 1 if dir == "R" else 1,
-            prev,
-        )
-        if update_best(*next):
-            heappush(check, next)
-    if dir != "R" and c > 0 and not (dir == "L" and speed == 3):
-        next = (
-            cost + city[r][c - 1],
-            r,
-            c - 1,
-            "L",
-            speed + 1 if dir == "L" else 1,
-            prev,
-        )
-        if update_best(*next):
-            heappush(check, next)
-
-result = float("inf")
-for speed in range(1, 4):
-    for dir in ("D", "R"):
-        key = len(city) - 1, len(city[0]) - 1, dir, speed
-        if key in best:
-            result = min(result, best[key][0])
+    if dir != "U":
+        update(cost, r + 1, c, "D", speed + 1 if dir == "D" else 1)
+    if dir != "D":
+        update(cost, r - 1, c, "U", speed + 1 if dir == "U" else 1)
+    if dir != "L":
+        update(cost, r, c + 1, "R", speed + 1 if dir == "R" else 1)
+    if dir != "R":
+        update(cost, r, c - 1, "L", speed + 1 if dir == "L" else 1)
 
 print(result)
