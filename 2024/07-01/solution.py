ae76147359814ea55c8316658bde9f5ff2880de1
Adrian Klaeger <adrian.klaeger@gmail.com>
2024-12-08 23:31:51 +0100

    2024 07

---
+++ 2024/07-01/solution.py
@@ -0,0 +1,20 @@
+with open("input") as f:
+    input = [line.strip() for line in f.readlines()]
+
+
+def calc(target, operands):
+    if len(operands) == 1:
+        return operands[0] == target
+    a, b, *rest = operands
+    return calc(target, [a + b] + rest) or calc(target, [a * b] + rest)
+
+
+result = 0
+for line in input:
+    target, rest = line.split(": ")
+    target = int(target)
+    operands = list(map(int, rest.split()))
+    if calc(target, operands):
+        result += target
+
+print(result)
