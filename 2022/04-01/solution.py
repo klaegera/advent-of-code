ca4cebe5d586631aa4f8ef67fc1956b70f106dd3
Adrian Klaeger <adrian.klaeger@gmail.com>
2024-12-13 22:42:58 +0100

    2022 04

---
+++ 2022/04-01/solution.py
@@ -0,0 +1,13 @@
+with open("input") as f:
+    input = [line.strip() for line in f.readlines()]
+
+import re
+
+pattern = re.compile(r"(\d+)-(\d+),(\d+)-(\d+)")
+
+result = 0
+for line in input:
+    a, b, c, d = map(int, re.match(pattern, line).groups())
+    result += a <= c and b >= d or c <= a and d >= b
+
+print(result)
