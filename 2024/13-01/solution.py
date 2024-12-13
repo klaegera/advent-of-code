9781c4db6f0a64f59960cf49c3cabce1d95bbac5
Adrian Klaeger <adrian.klaeger@gmail.com>
2024-12-13 13:03:19 +0100

    2024 13

---
+++ 2024/13-01/solution.py
@@ -0,0 +1,23 @@
+with open("input") as f:
+    input = [line.strip() for line in f.readlines()]
+
+import re
+
+result = 0
+it = iter(input)
+pattern = re.compile(r".*X.(\d+), Y.(\d+)")
+while True:
+    ax, ay = map(int, re.match(pattern, next(it)).groups())
+    bx, by = map(int, re.match(pattern, next(it)).groups())
+    px, py = map(int, re.match(pattern, next(it)).groups())
+
+    A = (by * px - bx * py) / (by * ax - bx * ay)
+    B = (px - ax * A) / bx
+
+    if A == int(A) and B == int(B):
+        result += int(3 * A + B)
+
+    if next(it, None) is None:
+        break
+
+print(result)
